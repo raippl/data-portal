b5e34c37c4f6ac50509c2ad20fc732e364f8d839
Alessandro Ercolani <alessandro@teamdigitale.governo.it>
2017-06-22 15:28:57 +0200

    First integration with ita-web-toolkit

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,6 +9,7 @@ class Header extends React.Component {
         <ul>
           <li><Link to="/">Home</Link></li>
           <li><Link to="/login">Login</Link></li>
+          <li><Link to="/ingestion">Ingestion</Link></li>
         </ul>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { BrowserRouter as Router, Route} from 'react-router-dom'
+//import registerServiceWorker from './registerServiceWorker';
 
 import Home from './components/Home';
 import Login from './components/Login';
+import IngestionForm from './ingestion-form/IngestionForm'
 import store from './store';
 
 ReactDOM.render((
@@ -13,7 +15,9 @@ ReactDOM.render((
         <div>
           <Route exact path="/" component={Home}/>
           <Route path="/login" component={Login}/>
+          <Route path="/ingestion" component={IngestionForm}/>
         </div>
       </Router>
   </Provider>
 ), document.getElementById('root'));
+//registerServiceWorker();
